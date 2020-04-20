5653078fc3eff752096efe1529f96c31e3dda06e
史 翔新 <x_shi@yumemi.co.jp>
2020-04-20 20:59:14 +0900

    Initial Xcode project

---
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 史 翔新 on 2020/04/20.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
