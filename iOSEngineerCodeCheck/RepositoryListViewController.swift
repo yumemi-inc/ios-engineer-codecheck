7cdfcb87e9d269c2411c4b6ca6372a5aebbccf86
史 翔新 <x_shi@yumemi.co.jp>
2020-04-21 21:44:41 +0900

    Search for repoositories in GitHub

--- iOSEngineerCodeCheck/RepositoryListViewController.swift
+++ iOSEngineerCodeCheck/RepositoryListViewController.swift
@@ -12,11 +12,35 @@ class RepositoryListViewController: UITableViewController, UISearchBarDelegate {
 
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var repositories: [[String: Any]] = []
+    
+    var dataTask: URLSessionTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         searchBar.delegate = self
     }
-
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        dataTask?.cancel()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let word = searchBar.text!
+        
+        if word.count != 0 {
+            let url = "https://api.github.com/search/repositories?q=\(word)"
+            dataTask = URLSession.shared.dataTask(with: URL(string: url)!) { (data, response, error) in
+                let object = try! JSONSerialization.jsonObject(with: data!) as! [String: Any]
+                let items = object["items"] as! [[String: Any]]
+                self.repositories = items
+                print(items)
+            }
+            dataTask?.resume()
+        }
+        
+    }
+    
 }
-
