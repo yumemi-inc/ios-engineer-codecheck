6b400889f13d7250fbc5747b0c030cae26690363
史 翔新 <x_shi@yumemi.co.jp>
2020-04-21 19:36:39 +0900

    Rename initial vc to RepositoryListViewController

--- iOSEngineerCodeCheck/RepositoryListViewController.swift
+++ iOSEngineerCodeCheck/RepositoryListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RepositoryListViewController.swift
 //  iOSEngineerCodeCheck
 //
 //  Created by 史 翔新 on 2020/04/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RepositoryListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
