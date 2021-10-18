797eb397d73fe188a12b04f4fc15fb5518d17806
Elvis Shi <shonetesmith@gmail.com>
2021-10-19 01:02:22 +0900

    Feature: Xcode 13 update (#12)
    * Update project settings with Xcode 13 recommendations
    
    * Bump deployment target version to iOS 15.0
    
    * Update environment descriptions in README
    
    * Add a link about how your work is reviewed in README
    
    * Remove deployment target specification for UnitTest target, so it can use project specification instead of target specification
    
    * Fix API usage due to system update

--- iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
+++ iOSEngineerCodeCheckUITests/iOSEngineerCodeCheckUITests.swift
@@ -35,7 +35,7 @@ class iOSEngineerCodeCheckUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
         }
